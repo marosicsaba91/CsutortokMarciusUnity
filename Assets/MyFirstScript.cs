e079aade0d4a127497d1b06b45b3af7e5f9f3ac5
Csaba Marosi <marosicsaba91@gmail.com>
2023-03-23 21:05:01 +0100

    My Frist Commit

---
+++ Assets/MyFirstScript.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+
+class MyFirstScript : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Hello, " + name);   // Kiíratás
+        Debug.Log("I'm Csabi");        /* Kiíratás */
+
+        int a;
+        a = 10;
+        a = 20;
+
+        Debug.Log(a);
+
+        int age = 31;
+        Debug.Log(age);
+
+        int x = 2, y = 3, z = 4;
+
+        int v1 = x + y;    // 5
+        int v2 = x - z;    // -2
+        int v3 = v1 * v2;  // -10
+        int v4 = y / x;    // 1
+
+        int v5 = 15 % 4;    // 3
+        int v6 = -v5;       // -3
+
+        // ---------------
+
+        v6 = v6 + 5;
+        v6 = x - v6;
+
+
+        x += 5;   // x = x + 5;
+        x -= 5;   // x = x - 5;
+        x *= 5;   // x = x * 5;
+        x /= 5;   // x = x / 5;
+        x %= 5;   // x = x % 5;
+
+
+        x++;      // x += 1;     x = x+1; 
+        x--;      // x = x - 1;
+
+        // -----------------------------------
+
+        x = 2;
+        y = 6;
+        z = 3;
+
+        int v7 = x + y * z;      // 20
+        int v8 = (x + y) * z;    // 24 
+        int v9 = x + (y * z);    // 20
+
+
+    }
+}
